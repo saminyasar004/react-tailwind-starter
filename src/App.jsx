e9b9644af003089b9195f06eb5d8f2c4bbc64833
saminyasar004 <yasarsamin57@gmail.com>
2021-12-13 19:21:49 +0600

    Initialized Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="bg-gray-800 w-full h-screen overflow-hidden cursor-default flex items-center justify-center">
+            <h2 className="text-green-500 capitalize text-xl">hello world!</h2>
+        </div>
+    );
+}
+
+export default App;
